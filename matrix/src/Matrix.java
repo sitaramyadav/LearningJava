69a310854db7adff9d24fdd6a5ce8a322aae05c7
sitaram <sitaram@thoughtworks.com>
2016-02-25 10:43:30 +0530

    multiply added.
    What changes have you made?
    
    *It can multiply of tw matrix.
    
    Why do we need this change?
    
    * for multiplying the two matrix.
    
    What can this change impact?
    
    *nothing.

--- matrix/src/Matrix.java
+++ matrix/src/Matrix.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;
 public class Matrix {
-   private int row,colom;
+   private int row,column;
    private int[][] matrix;
-   public Matrix(int row,int colom){
+   public Matrix(int row,int column){
       this.row = row;
-      this.colom = colom;
-      this.matrix = new int[row][colom];
+      this.column = column;
+      this.matrix = new int[row][column];
    }
    public void fill(int[] ele){
       int k=0;
       for(int i =0;i<row;i++){
-         for(int j=0;j<colom;j++){
+         for(int j=0;j<column;j++){
             this.matrix[i][j]=ele[k++];
          }
       }
@@ -18,4 +18,45 @@ public class Matrix {
    public boolean isEqual(Matrix matrix){
      return Arrays.deepEquals(matrix.matrix,this.matrix);
    }
+
+   // public String toString(){
+   //   String result="";
+   //   for (int i=0;i<this.row;i++) {
+   //     for (int j=0;j<this.column;j++) {
+   //       result = result+this.matrix[i][j]+" ";
+   //     }
+   //     result = result +" \n";
+   //   }
+   //   return result;
+   // }
+    public Matrix add(Matrix _matrix) {
+     Matrix result = new Matrix(this.row ,this.column);
+     int[] result_ = new int[row*column];
+     if(!(_matrix.row ==this.row && _matrix.column ==this.column))
+       return new Matrix(0,0);
+     for (int k=0,i=0;i<this.row;i++) {
+       for (int j=0;j<this.column ;j++) {
+         result_[k++]=this.matrix[i][j]+_matrix.matrix[i][j];
+       }
+     }
+     result.fill(result_);
+     return result;
+   }
+  public Matrix multiply(Matrix matrix){
+    Matrix result = new Matrix(this.row,matrix.column);
+    int[] result_ = new int[this.row*matrix.column];
+    if(!(this.row==matrix.row && this.column==matrix.column))
+      return new Matrix(0,0);
+    for (int i=0;i< this.row;i++){
+      for (int l=0,j=0;j< this.column ;j++) {
+          int multiplication = 0;
+        for (int k = 0; k<matrix.column;k++) {
+          multiplication=this.matrix[i][k]*matrix.matrix[k][j]+multiplication;
+        }
+          result_[l++] = multiplication;
+      }
+    }
+    result.fill(result_);
+    return result;
+  }
 }
